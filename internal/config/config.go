2bc53ff1105264bebc6ea96fa3556e1b808434b6
Yuval dekel <153660450+yuvaldekel@users.noreply.github.com>
2026-02-11 21:11:59 +0200

    add validator

--- internal/config/config.go
+++ internal/config/config.go
@@ -26,6 +26,7 @@ import (
 	"github.com/alecthomas/kingpin/v2"
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
+	"github.com/go-playground/validator/v10"
 )
 
 
