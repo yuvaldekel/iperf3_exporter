d1fef50e51cb4a5cf308240af7ad93516bec4890
Yuval dekel <153660450+yuvaldekel@users.noreply.github.com>
2026-02-14 09:10:50 +0200

    info to Info

--- internal/collector/collector.go
+++ internal/collector/collector.go
@@ -248,11 +248,11 @@ func (c *Collector) Collect(ch chan<- prometheus.Metric) {
 		ch <- prometheus.MustNewConstMetric(c.receivedSeconds, prometheus.GaugeValue, result.ReceivedSeconds, labelValues...)
 		ch <- prometheus.MustNewConstMetric(c.receivedBytes, prometheus.GaugeValue, result.ReceivedBytes, labelValues...)
 
-		c.logger.info("", result.Protocol)
+		c.logger.Info("", result.Protocol)
 
 		// Retransmits is only relevant in TCP protocol
 		if result.Protocol == "tcp" {
-			c.logger.info("", result.Protocol)
+			c.logger.Info("", result.Protocol)
 			ch <- prometheus.MustNewConstMetric(c.retransmits, prometheus.GaugeValue, result.Retransmits, labelValues...)
 		}
 
